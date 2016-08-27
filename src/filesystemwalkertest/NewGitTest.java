a9c3295e18be82c7c955fa7b69804d1c9b992e7e
anilcha <anilcha@ANILCHA-IN.oradev.oraclecorp.com>
2016-08-27 19:56:56 +0530

    FSWalker commit 2

---
+++ src/filesystemwalkertest/NewGitTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesystemwalkertest;
+
+/**
+ *
+ * @author anilcha
+ */
+public class NewGitTest {
+    
+    
+    public String sayHello(){
+        return "Hello";
+    }
+    
+}
